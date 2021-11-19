e2953cdac216029d23d402fd45a4e1eec4315406
David <74385834+Dave-JunDev@users.noreply.github.com>
2021-11-18 20:59:23 -0500

    Add files via upload

---
+++ src/services/getGifs.js
@@ -0,0 +1,21 @@
+const apiKey = ""
+
+
+export default function getGifs({keyword = ''} = {} ) {
+
+const ApiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
+
+    return fetch(ApiURL)
+   .then(res => res.json())
+   .then(response =>{
+      const {data} = response;
+      if (Array.isArray(data)) {
+          const gifs = data.map(image => {
+            const {images, title, id} = image
+            const {url} = images.downsized_medium
+            return {title, url, id}
+      })
+      return gifs;
+    }
+   })
+};
