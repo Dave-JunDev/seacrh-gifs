1c06a03523893edb0edc9f0094cc0a78c95fd612
Carlos <carlos.pinilla@vmocentral.com>
2022-11-30 22:16:53 -0500

    Modificar filtro de la cantidad de gift en pantalla

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, {useState} from 'react';
-import './App.css';
-import ListOfGifs from './componentes/listofGifs'
-
+import React, { useState } from "react";
+import "./App.css";
+import ListOfGifs from "./componentes/listofGifs";
 
 function App() {
+	const [keyword, setKeyword] = useState();
 
-  const [keyword, setKeyword] = useState();
-
-  return (
-    <div className="App">
-      <h1>Gif Search</h1>
-      <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder="Que deseas buscar?"/>
-      <section className="App-content">
-          <ListOfGifs keyword={keyword} />
-      </section>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Gif Search</h1>
+			<input
+				type="text"
+				value={keyword}
+				onChange={(e) => setKeyword(e.target.value)}
+				placeholder="Que deseas buscar?"
+			/>
+			<section className="App-content">
+				<ListOfGifs keyword={keyword} numGifs={5} />
+			</section>
+		</div>
+	);
 }
 
 export default App;
