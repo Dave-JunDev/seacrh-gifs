88ab6c31208cfde3c6c5f4567ce76145a6550d1d
David Steven <david.steven.97.t@gmail.com>
2023-03-22 15:17:34 -0500

    comentario de la key

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,19 +1,19 @@
-const apiKey = "MgbAc48XyG1EQOju9ewsr66BqtoQeI8A";
+const apiKey = "MgbAc48XyG1EQOju9ewsr66BqtoQeI8A"; // Apikey de giphy develop
 
 export default function getGifs({ keyword = "", numGifs = 10 } = {}) {
 	const ApiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=${numGifs}&offset=0&rating=g&lang=en`;
 
 	return fetch(ApiURL)
 		.then((res) => res.json())
-		.then((response) => {
-			const { data } = response;
-			if (Array.isArray(data)) {
-				const gifs = data.map((image) => {
-					const { images, title, id } = image;
-					const { url } = images.downsized_medium;
-					return { title, url, id };
-				});
-				return gifs;
-			}
-		});
+		  .then((response) => {
+			  const { data } = response;
+			  if (Array.isArray(data)) {
+				    const gifs = data.map((image) => {
+					  const { images, title, id } = image;
+					  const { url } = images.downsized_medium;
+					  return { title, url, id };
+				  });
+				  return gifs;
+			  }
+		  });
 }
